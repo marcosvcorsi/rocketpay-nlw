1f0aa07f190dbcc1bb744bf90b48b5b76a92c8da
Marcos Corsi <marcosvcorsi@gmail.com>
2021-02-22 20:49:09 -0300

    feat: add sum from file to welcom controller

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -9,11 +9,11 @@ defmodule Rocketpay.Numbers do
     result =
       result
       |> String.split(",")
-      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Stream.map(fn number -> String.to_integer(number) end)
       |> Enum.sum()
 
     {:ok, %{result: result}}
   end
 
-  defp handle_file({:error, _reason}), do: {:error, "Invalid File!"}
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
